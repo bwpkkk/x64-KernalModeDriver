59357c9a294cb3160faf91c657de3517f61f669b
0xE8 <82914235+bwpkkk@users.noreply.github.com>
2023-02-06 13:53:41 -0500

    Delete DriverInjector directory

--- DriverInjector/IRP.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include<ntifs.h> //ntddk.h
-#include "myfunction.h"
-
-#include <ntstatus.h>
-HANDLE Pid;
-NTSTATUS IRP_CALL(PDEVICE_OBJECT device, PIRP pirp);
--- DriverInjector/PageTableSplit.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<ntifs.h>
-
-
-
-ULONG64 GetPML4(ULONG64 linearAddress);
-
-PVOID AllocateMemory(HANDLE pid, SIZE_T size);
